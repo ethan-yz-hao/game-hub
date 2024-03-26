833eecdbbcb6a34eba8b04a8603d2de2923c2546
Ethan Hao <hao.yz.ethan@gmail.com>
2024-03-26 19:46:49 -0400

    clean up the genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -16,8 +16,8 @@ const GameCard = ({game}: Props) => {
             <Image src={getCroppedImageURL(game.background_image)}/>
             <CardBody>
                 <HStack justifyContent="space-between" marginBottom={3}>
-                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-                    <CriticScore score={game.metacritic} />
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+                    <CriticScore score={game.metacritic}/>
                 </HStack>
                 <Heading fontSize="2xl">{game.name}</Heading>
             </CardBody>
